6fa3a13b68f0fa83d0cb02b807aee4f8eaf31d46
Dacaremi90 <81193871+Dacaremi90@users.noreply.github.com>
2021-04-08 16:25:54 -0300

    Entrega ejercicios Clase 3

---
+++ Clase3.Ejercicio1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> values = new Dictionary<string, string>();
+            values.Add("Juan","55423412");
+            values.Add("Hernesto","56985623");
+            values.Add("Mariana","54787451");
+
+            if(values.ContainsKey("Juan") == true)
+            {
+                Console.WriteLine(values["Juan"]);
+            }
+
+
+            string value;
+
+            if(values.TryGetValue("Pedro", out value))
+            {
+                Console.WriteLine("For key = \"Pedro\", value = {0} ",value);
+            }
+            else
+            {
+                Console.WriteLine("No contiene la llave");
+            }
+
+            foreach (KeyValuePair<string, string> item in values)
+            {
+                Console.WriteLine("{0} , {1}", item.Key, item.Value);
+            }
+
+            values.Remove("Mariana");
+            values.Add("Mariana", "58251425");
+
+            foreach (KeyValuePair<string, string> item in values)
+            {
+                if (item.Key == "Mariana")
+                {
+                    Console.WriteLine("{0},  Nuevo telefono de Mariana = {1}", item.Key, item.Value);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase3.Ejercicio2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio2
+{
+    class Program
+    {
+        private static readonly string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+        private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+
+
+        static void Main(string[] args)
+        {
+            List<string> colores = new List<string>();
+            List<string> removerColores = new List<string>();
+
+
+
+            for (int i = 0; i <= colors.Length - 1; i++)
+            {
+                colores.Add(colors[i]);
+
+
+            }
+
+            for (int i = 0; i <= removeColors.Length - 1; i++)
+            {
+                removerColores.Add(removeColors[i]);
+
+            }
+
+            /*Mostrar colores por pantalla*/
+            Console.WriteLine("Ejercicio B");
+
+            foreach (string e in colores)
+            {
+                Console.Write(e + " ");
+            }
+
+
+
+            for (int i = 0; i <= removerColores.Count - 1; i++)
+            {
+                colores.Remove(removerColores.ElementAt(i));
+
+            }
+
+
+            /*Volver a mostrar lista*/
+            Console.WriteLine("\nColores: ");
+            foreach (string e in colores)
+            {
+                Console.Write(e + " ");
+            }
+
+        }
+        
+    }
+
+    
+}
+
